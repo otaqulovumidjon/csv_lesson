bcf51db64c405a8043c7441972187797037c53a7
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-09 11:28:20 +0500

    country names

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ def get_country_column(file_name):
     Returns:
         list
     """
-    return 0
+    ls = []
+    f = open(file_name).read()
+    n = f.split("\n")
+    n.pop(0)
+    for i in n:
+        if len(i) != 0:
+            a = i.split(",")
+            ls.append(a[3])
+    return ls
+print(get_country_column("data.csv"))
